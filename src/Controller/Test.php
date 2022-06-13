970c6178ab68fb6df47708731473ed0ed2ce4b11
Marko Ivančić <marko.ivancic@srce.hr>
2022-06-13 14:54:50 +0200

    WIP

--- src/Controller/Test.php
+++ src/Controller/Test.php
@@ -2,10 +2,40 @@
 
 namespace SimpleSAML\Module\accounting\Controller;
 
+use SimpleSAML\Configuration;
+use SimpleSAML\Locale\Translate;
+use SimpleSAML\Session;
+use SimpleSAML\XHTML\Template;
+use Symfony\Component\HttpFoundation\Request;
+
 class Test
 {
-    public function test(): void
+    protected Configuration $configuration;
+    protected Session $session;
+
+    /**
+     * @param Configuration $configuration
+     * @param Session $session The current user session.
+     */
+    public function __construct(Configuration $configuration, Session $session)
     {
-        echo 'test';
+        $this->configuration = $configuration;
+        $this->session = $session;
+    }
+
+    /**
+     * @param Request $request
+     * @return Template
+     * @throws \Exception
+     */
+    public function test(Request $request): Template
+    {
+        $template = new Template(Configuration::getConfig(), 'accounting:configuration.twig');
+
+        $template->data = [
+            'test' => Translate::noop('Accounting'),
+        ];
+
+        return $template;
     }
 }
