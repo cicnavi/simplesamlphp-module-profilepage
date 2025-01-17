37e0d835d8ad00229ac2a09dbc4b5d2be9fced7e
Marko Ivancic <mivanci@srce.hr>
2025-01-17 15:11:07 +0100

    Update rector to v2

--- rector.php
+++ rector.php
@@ -2,32 +2,17 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/hooks',
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->skip([
-        __DIR__ . '/src/Services/JobRunner.php',
-        //__DIR__ . '/src/WholeDirectory',
-
-        // or use fnmatch
-        //__DIR__ . '/src/*/Tests/*',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-    $rectorConfig->rule(DeclareStrictTypesRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81
-    ]);
-};
+    ])
+    // uncomment to reach your current PHP version
+    ->withPhpSets()
+    ->withTypeCoverageLevel(1000)
+    ->withDeadCodeLevel(1000)
+    ->withCodeQualityLevel(1000)
+    ;
